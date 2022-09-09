4ea7614e54f1e39c2bf0ac14eb7d51a8317c4d4a
santiagomallama08 <111613750+santiagomallama08@users.noreply.github.com>
2022-09-08 20:07:31 -0500

    Add files via upload

---
+++ ejercicio5.cs
@@ -0,0 +1,11 @@
+﻿namespace ejercicio5
+{
+    public class program
+    {
+        public static void Main()
+        {
+            double nume2;
+            for(nume2=0; nume2 <10; nume2++ )
+        }
+    }
+}
